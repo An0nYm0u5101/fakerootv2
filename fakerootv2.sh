6e1183a004bd1c6473141e952f50ea83593d2689
MrPawNO <powermiyako@gmail.com>
2019-06-10 12:44:25 +0700

    Add files via upload

---
+++ fakerootv2.sh
@@ -0,0 +1,13 @@
+clear
+    
+    echo "MEMULAI FAKEROOT..."
+    echo "========== FAKEROOT BY Mr.PawN0 =========="
+    echo "========== FAKEROOT AUTOSAVE    =========="
+    echo
+    echo
+    echo
+    apt install proot
+    proot -0 -w ~ $PREFIX/bin/bash
+    proot -0 -w ~ $PREFIX/bin/bash
+    echo "SELESAI..."
+    clear
